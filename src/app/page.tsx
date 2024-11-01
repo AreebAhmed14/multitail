d12822b2dd4fa57c3a6e1509fbbc192ad78e4e37
AreebAhmed14 <areebfarooq549@gmail.com>
2024-11-01 03:32:38 -0700

    fix header bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ const Page = () => {
         <div className='absolute inset-0 bg-[url("/static/download.jpg")] bg-cover blur-sm shadow-2xl'></div>
         <div className=" flex justify-center items-center relative">
           <center>
-          <h1 className="text-5xl max-[590px]:text-4xl max-[400px]:text-[2rem] w-[80%] max-[590px]:w-[95%] text-black font-sans font-bold shadow-inner">
-          <span className="text-white">JOIN</span> <span className="text-white rounded-md py-0 bg-black px-3">FITNESS</span> HUB WHERE <span className="text-red-800">EVERY</span> <span className="text-white">WORK</span>OUT <span className="text-white">IS</span> STEP <span className="text-red-800">TOWARD</span> <span className="text-white ">GREATNESS</span>
+          <h1 className="text-5xl max-[590px]:text-4xl max-[400px]:text-[2rem] w-[80%] max-[590px]:w-[95%] text-white font-sans font-bold shadow-inner">
+            JOIN <span className="text-black">FITNESS</span><span className="text-red-800">HUB</span> WHERE EVERY <span className="text-black">WORKOUT</span> IS STEP TOWARD <span className="text-red-800">GEARTNESS</span>.
           </h1>
           </center>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,96 @@
-"use client"
-import React from 'react'
-import Link from 'next/link'
-import { TiThMenu } from "react-icons/ti";
-
-// import { ImCross } from "react-icons/im";
-// import { useState } from 'react';
-
-
+"use client";
+import React, { useState } from "react";
+import { TiThMenuOutline } from "react-icons/ti";
+import Link from "next/link";
+import { ImCross } from "react-icons/im";
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    {
+      if (isMenuOpen == false) {
+        setIsMenuOpen(true);
+      } else {
+        setIsMenuOpen(false);
+      }
+    }
+  };
+  function remove(){
+    setIsMenuOpen(false);
+  }
+
   return (
-    <>
-    <div className='relative w-full h-[14vh]'>
-      <div className='w-full h-[14vh] bg-black absolute blur-xl'></div>
-    <div className='w-full absolute h-[14vh] flex justify-between items-center'>
-      <h1 className='ml-5 text-red-800 text-4xl font-bold font-sans max-[400px]:text-3xl'>FITNESS <span className='text-white'>HUB</span></h1>
-      <ul className='flex mr-5 max-[670px]:hidden'>
-        <Link href={"/"}><li className='m-5 max-[780px]:m-2 font-sans text-xl font-bold text-white hover:scale-125 cursor-pointer transition-all duration-[0.5s] hover:text-red-900'>Home</li></Link>
-        <Link href={"/About"}><li className='m-5 max-[780px]:m-2 font-sans text-xl font-bold text-white hover:scale-125 cursor-pointer transition-all duration-[0.5s] hover:text-red-900'>About</li></Link>
-        <Link href={"/Pakages"}><li className='m-5 max-[780px]:m-2 font-sans text-xl font-bold text-white hover:scale-125 cursor-pointer transition-all duration-[0.5s] hover:text-red-900'>Membership</li></Link>
-        <Link href={"/Contact"}><li className='m-5 max-[780px]:m-2 font-sans text-xl font-bold text-white hover:scale-125 cursor-pointer transition-all duration-[0.5s] hover:text-red-900'>Contact</li></Link>
+    <div
+      id="home"
+      className="w-full h-[15vh] flex justify-between items-center bg-black"
+    >
+      <h1 className="ml-5 text-[2.5rem] max-[809px]:text-[2.1rem] max-[400px]:text-[2rem] max-[400px]:ml-3 max-[350px]:text-[1.8rem] font-sans text-white font-bold">
+        <span className="text-red-800">FIT</span>NESS{" "}
+        <span className="text-red-800">HUB</span>
+      </h1>
+      <ul className="flex m-[1rem] max-[790px]:hidden">
+        <Link href={"/"}>
+          <li className="m-5 text-[1.5rem] transition-all hover:scale-[1.2] hover:text-red-800 duration-700 cursor-pointer font-sans font-bold max-[930px]:text-[1.1rem] text-white">
+            Home
+          </li>
+        </Link>
+        <Link href={"/About"}>
+          <li className="m-5 text-[1.5rem] transition-all hover:scale-[1.2] hover:text-red-800 duration-700 cursor-pointer font-sans font-bold max-[930px]:text-[1.1rem] text-white">
+            About
+          </li>
+        </Link>
+        <Link href={"/Pakages"}>
+          <li className="m-5 text-[1.5rem] transition-all hover:scale-[1.2] hover:text-red-800 duration-700 cursor-pointer font-sans font-bold max-[930px]:text-[1.1rem] text-white">
+            Memberships
+          </li>
+        </Link>
+        <Link href={"/Contact"}>
+          <li className="m-5 text-[1.5rem] transition-all hover:scale-[1.2] hover:text-red-800 duration-700 cursor-pointer font-sans font-bold max-[930px]:text-[1.1rem] text-white">
+            Contact
+          </li>
+        </Link>
       </ul>
+
+      {/* Mobile Menu Icon */}
+      <TiThMenuOutline
+        onClick={toggleMenu}
+        className="text-[2.2rem] font-bold hidden max-[790px]:block max-[400px]:text-[1.9rem] max-[350px]:text-[1.7rem] cursor-pointer mr-5 text-white"
+      />
+
+      <div
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } z-10 w-full h-[100vh] transition-all duration-[2s] bg-black absolute top-0 left-0`}
+      >
+        <div className="w-full h-[15vh] flex justify-end items-center bg-black bg-opacity-40 border-b-[0.3rem] border-red-800">
+          <ImCross onClick={toggleMenu} className="mr-6 text-2xl text-white" />
+        </div>
+
+        <ul className="flex justify-center items-center flex-col">
+          <Link href={"/"}>
+            <li onClick={remove} className="w-full h-[15vh] flex justify-center items-center font-bold text-white border-b-[0] border-white my-3.5 text-2xl font-sans">
+              HOME
+            </li>
+          </Link>
+          <Link href={"/About"}>
+            <li onClick={remove} className="w-full h-[15vh] flex justify-center items-center font-bold text-white border-b-[0] border-white my-3.5 text-2xl font-sans">
+              ABOUT
+            </li>
+          </Link>
+          <Link href={"/Pakages"}>
+            <li onClick={remove} className="w-full h-[15vh] flex justify-center items-center font-bold text-white border-b-[0] border-white my-3.5 text-2xl font-sans">
+              MEMBERSHIPS
+            </li>
+          </Link>
+          <Link href={"/Contact"}>
+            <li onClick={remove} className="w-full h-[15vh] flex justify-center items-center font-bold text-white border-b-[0] border-white my-3.5 text-2xl font-sans">
+              CONTACT
+            </li>
+          </Link>
+        </ul>
       </div>
-      <Link href={"/Menu"}> <TiThMenu className='cursor-pointer absolute right-0 top-4 hidden mr-6 text-red-800 text-3xl max-[670px]:block' /></Link>
-
     </div>
-    
-    </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
